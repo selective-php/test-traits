3d732d64424c3c8e44257437cf650662814b1cc6
Daniel Opitz <d.opitz@outlook.com>
2021-01-24 01:08:43 +0100

    Init

--- src/Traits/DatabaseTestTrait.php
+++ src/Traits/DatabaseTestTrait.php
@@ -215,7 +215,11 @@ trait DatabaseTestTrait
         array $fields = null,
         string $message = ''
     ): void {
-        $this->assertSame($expectedRow, $this->getRowById($table, $id, $fields ?: array_keys($expectedRow)), $message);
+        $this->assertSame(
+            $expectedRow,
+            $this->getTableRowById($table, $id, $fields ?: array_keys($expectedRow)),
+            $message
+        );
     }
 
     /**
@@ -238,7 +242,7 @@ trait DatabaseTestTrait
     ): void {
         $this->assertEquals(
             $expectedRow,
-            $this->getRowById($table, $id, $fields ?: array_keys($expectedRow)),
+            $this->getTableRowById($table, $id, $fields ?: array_keys($expectedRow)),
             $message
         );
     }
@@ -261,7 +265,7 @@ trait DatabaseTestTrait
         string $field,
         string $message = ''
     ): void {
-        $actual = $this->getRowById($table, $id, [$field])[$field];
+        $actual = $this->getTableRowById($table, $id, [$field])[$field];
         $this->assertSame($expected, $actual, $message);
     }
 
@@ -280,6 +284,53 @@ trait DatabaseTestTrait
     }
 
     /**
+     * Asserts that a given table contains a given number of rows.
+     *
+     * @param string $table Table to look into
+     * @param int $id The id
+     * @param string $message Optional message
+     *
+     * @return void
+     */
+    protected function assertTableRowExists(string $table, int $id, string $message = ''): void
+    {
+        $this->assertTrue((bool)$this->findTableRowById($table, $id), $message);
+    }
+
+    /**
+     * Asserts that a given table contains a given number of rows.
+     *
+     * @param string $table Table to look into
+     * @param int $id The id
+     * @param string $message Optional message
+     *
+     * @return void
+     */
+    protected function assertTableRowNotExists(string $table, int $id, string $message = ''): void
+    {
+        $this->assertFalse((bool)$this->findTableRowById($table, $id), $message);
+    }
+
+    /**
+     * Fetch row by ID.
+     *
+     * @param string $table Table name
+     * @param int $id The primary key value
+     *
+     * @throws DomainException
+     *
+     * @return array<mixed> Row
+     */
+    protected function findTableRowById(string $table, int $id): array
+    {
+        $sql = sprintf('SELECT * FROM `%s` WHERE `id` = :id', $table);
+        $statement = $this->createPreparedStatement($sql);
+        $statement->execute(['id' => $id]);
+
+        return $statement->fetch(PDO::FETCH_ASSOC) ?: [];
+    }
+
+    /**
      * Fetch row by ID.
      *
      * @param string $table Table name
@@ -290,7 +341,7 @@ trait DatabaseTestTrait
      *
      * @return array<mixed> Row
      */
-    protected function getRowById(string $table, int $id, array $fields = null): array
+    protected function getTableRowById(string $table, int $id, array $fields = null): array
     {
         $sql = sprintf('SELECT * FROM `%s` WHERE `id` = :id', $table);
         $statement = $this->createPreparedStatement($sql);
@@ -299,7 +350,7 @@ trait DatabaseTestTrait
         $row = $statement->fetch(PDO::FETCH_ASSOC);
 
         if (empty($row)) {
-            throw new DomainException(__('Row not found: %s', $id));
+            throw new DomainException(sprintf('Row not found: %s', $id));
         }
 
         if ($fields) {
