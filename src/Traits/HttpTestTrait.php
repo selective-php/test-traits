fd235abc5b12dab171cdef63663a8c0871453f8d
Daniel Opitz <d.opitz@outlook.com>
2021-02-12 23:13:51 +0100

    Add RouteTestTrait

--- src/Traits/HttpTestTrait.php
+++ src/Traits/HttpTestTrait.php
@@ -44,18 +44,4 @@ trait HttpTestTrait
 
         return $request->withHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
-
-    /**
-     * Build the path for a named route including the base path.
-     *
-     * @param string $routeName Route name
-     * @param string[] $data Named argument replacement data
-     * @param string[] $queryParams Optional query string parameters
-     *
-     * @return string route with base path
-     */
-    protected function urlFor(string $routeName, array $data = [], array $queryParams = []): string
-    {
-        return $this->app->getRouteCollector()->getRouteParser()->urlFor($routeName, $data, $queryParams);
-    }
 }
