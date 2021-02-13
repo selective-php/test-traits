014f1822c248e4de42b7a5d5d8c96219d15fa7d0
Daniel Opitz <d.opitz@outlook.com>
2021-02-13 11:29:14 +0100

    Add Container trait setContainerValue method

--- src/Traits/ContainerTestTrait.php
+++ src/Traits/ContainerTestTrait.php
@@ -2,6 +2,7 @@
 
 namespace Selective\TestTrait\Traits;
 
+use BadMethodCallException;
 use Psr\Container\ContainerInterface;
 use UnexpectedValueException;
 
@@ -35,5 +36,24 @@ trait ContainerTestTrait
         }
 
         throw new UnexpectedValueException('Container must be initialized');
+    }
+
+    /**
+     * Define an object or a value in the container.
+     *
+     * @param string $name Entry name
+     * @param mixed $value Value, use definition helpers to define objects
+     *
+     * @throws BadMethodCallException
+     *
+     * @return void
+     */
+    protected function setContainerValue(string $name, $value): void
+    {
+        if (method_exists($this->container, 'set')) {
+            $this->container->set($name, $value);
+        }
+
+        throw new BadMethodCallException('This DI container does not support this feature');
     }
 }
