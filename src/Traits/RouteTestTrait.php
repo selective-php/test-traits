31094a6bb652cbffc722090c76372ca0c31487c0
Daniel Opitz <d.opitz@outlook.com>
2022-02-09 12:58:12 +0100

    Add RouteTestTrait #1

--- src/Traits/RouteTestTrait.php
+++ src/Traits/RouteTestTrait.php
@@ -14,7 +14,10 @@ trait RouteTestTrait
      *
      * @param string $routeName Route name
      * @param string[] $data Named argument replacement data
-     * @param string[] $queryParams Optional query string parameters
+     * @param string[] $queryParams Optional query string parameters.
+     * If you're using `nyholm/psr7`, query parameters MUST be added via
+     * `$request = $request->withQueryParams($queryParams)`
+     * to be retrieved with `$request->getQueryParams();`
      *
      * @return string The route with base path
      */
