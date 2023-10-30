0bd31abd92cd389c857af9ef8951b1c7f3f8a9a1
Samuel Gfeller <31797204+samuelgfeller@users.noreply.github.com>
2023-10-30 12:31:47 +0100

    Removed nyholm/psr7 warning
    It seems that query string parameters passed via arguments finally work with nyholm/psr7.
    
    https://github.com/Nyholm/psr7/pull/214

--- src/Traits/RouteTestTrait.php
+++ src/Traits/RouteTestTrait.php
@@ -15,9 +15,6 @@ trait RouteTestTrait
      * @param string $routeName Route name
      * @param string[] $data Named argument replacement data
      * @param string[] $queryParams Optional query string parameters.
-     * If you're using `nyholm/psr7`, query parameters MUST be added via
-     * `$request = $request->withQueryParams($queryParams)`
-     * to be retrieved with `$request->getQueryParams();`
      *
      * @return string The route with base path
      */
